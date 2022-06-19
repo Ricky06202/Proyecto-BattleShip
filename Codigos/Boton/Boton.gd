023a58e690239164b3a35a50deaa0c068a856bf9
Ricardo A Sanjur G <91886986+Ricky06202@users.noreply.github.com>
2022-06-19 18:58:19 -0500

    Hicimos los Botones del menu del modo de juego y les aplicamos animaciones al pasarle el mouse y al darle clic

--- Codigos/Boton/Boton.gd
+++ Codigos/Boton/Boton.gd
@@ -1,16 +1,33 @@
 extends Area2D
 
-export (Texture)var imagen
-# Declare member variables here. Examples:
-# var a: int = 2
-# var b: String = "text"
+export (Texture) var imagenNormal
+export (Texture) var imagenSeleccionada
+export (Texture) var imagenClic
 
+var tamanoSeleccionada := Vector2(1.1,1.1)
+var tamanoNormal := Vector2(1,1)
+
+signal Seleccionado
+signal Deseleccionado
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	$Imagen.texture = imagen
+	$Imagen.texture = imagenNormal
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta: float) -> void:
-#	pass
+
+func _on_Boton_mouse_entered() -> void:
+	$Imagen.texture = imagenSeleccionada
+
+
+func _on_Boton_mouse_exited() -> void:
+	$Imagen.texture = imagenNormal
+
+
+func _on_Boton_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+	if event is InputEventMouseButton:
+		if event.is_action_pressed("clic"):
+			$Imagen.texture = imagenClic
+		else:
+			$Imagen.texture = imagenSeleccionada
+		
