b8762331033a572fc995fc925ea8d93c5c86ed66
Ricardo A Sanjur G <91886986+Ricky06202@users.noreply.github.com>
2022-06-21 18:27:34 -0500

    Arregle la animacion del boton y arregle algunas texturas asi como reposicione los barquitos

--- Codigos/Boton/Boton.gd
+++ Codigos/Boton/Boton.gd
@@ -4,11 +4,8 @@ export (Texture) var imagenNormal
 export (Texture) var imagenSeleccionada
 export (Texture) var imagenClic
 
-var tamanoSeleccionada := Vector2(1.1,1.1)
+var tamanoGrande := Vector2(1.1,1.1)
 var tamanoNormal := Vector2(1,1)
-
-signal Seleccionado
-signal Deseleccionado
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -18,16 +15,25 @@ func _ready() -> void:
 
 func _on_Boton_mouse_entered() -> void:
 	$Imagen.texture = imagenSeleccionada
+	$Imagen.scale = tamanoGrande
+	
 
 
 func _on_Boton_mouse_exited() -> void:
 	$Imagen.texture = imagenNormal
+	$Imagen.scale = tamanoNormal
+	
 
 
 func _on_Boton_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
 	if event is InputEventMouseButton:
 		if event.is_action_pressed("clic"):
 			$Imagen.texture = imagenClic
+			$Imagen.scale = tamanoNormal
+			
 		else:
 			$Imagen.texture = imagenSeleccionada
+			$Imagen.scale = tamanoGrande
+			
+			
 		
--- Codigos/Modo de juego/Modo_de_juego.gd
+++ Codigos/Modo de juego/Modo_de_juego.gd
@@ -1,11 +1,6 @@
 extends Node2D
 
 
-# Declare member variables here. Examples:
-# var a: int = 2
-# var b: String = "text"
-
-
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	$AnimacionSol.play("Sol_Quieto")
@@ -17,3 +12,9 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta: float) -> void:
 #	pass
+
+
+func _on_Menu_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+	if event is InputEventMouseButton:
+		if event.is_action_released("clic"):
+			get_tree().change_scene("res://Escenas/Menu/Menu.tscn")
