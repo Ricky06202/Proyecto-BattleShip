a1ffd7d3f160fee4bca3d88413fb8a92bb4796a4
Ricardo A Sanjur G <91886986+Ricky06202@users.noreply.github.com>
2022-06-21 19:42:25 -0500

    Agrege nuevas funcionalidades al boton

--- Codigos/Boton/Boton.gd
+++ Codigos/Boton/Boton.gd
@@ -4,12 +4,37 @@ export (Texture) var imagenNormal
 export (Texture) var imagenSeleccionada
 export (Texture) var imagenClic
 
+export (bool) var conTexto
+
+export (String) var texto
+export (DynamicFontData) var tipoDeLetra
+export (int) var tamanoLetra
+export (bool) var todoMayuscula
+export (Color) var colorDeLetra
+export (int) var tamanoDeContorno
+export (Color) var colorDeContorno
+export (bool) var usarFiltro
+
+func nuevaLetra() -> DynamicFont:
+	var letra := DynamicFont.new()
+	letra.size = tamanoLetra
+	letra.outline_size = tamanoDeContorno
+	letra.outline_color = colorDeContorno
+	letra.use_filter = usarFiltro
+	letra.font_data = tipoDeLetra
+	return letra
+
+
 var tamanoGrande := Vector2(1.1,1.1)
 var tamanoNormal := Vector2(1,1)
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	$Imagen.texture = imagenNormal
+	$Texto.visible = conTexto
+	$Texto.set("custom_fonts/font", nuevaLetra())
+	$Texto.uppercase = todoMayuscula
+	$Texto.text = texto
 
 
 
