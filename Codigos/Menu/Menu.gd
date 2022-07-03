ba04ce2126f1aa98718711d64ced6b2b1aa3d3aa
Thais C. Samudio A <samudioarauz@gmail.com>
2022-07-03 15:55:13 -0500

    Se agregaron las animaciones a los barcos, el cual forma parte del menu principal; a su vez se insertaron, animaron los botones y se les agregó su funcionalidad

--- Codigos/Menu/Menu.gd
+++ Codigos/Menu/Menu.gd
@@ -1,4 +1,4 @@
-extends Control
+extends Node2D
 
 
 # Declare member variables here. Examples:
@@ -8,7 +8,8 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$Animaciones.play("Titulo")
+	$AnimacionesTitulo.play("Titulo")
+	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -20,4 +21,16 @@ func _ready():
 	#for fuegoArtificial in get_tree().get_nodes_in_group("FuegosArtificiales"):
 		#(fuegoArtificial as CPUParticles2D).emitting = true
 
+func _on_AnimacionesTitulo_animation_finished(anim_name):
+	$AnimacionesBarcos.play("Barcos")
+	$AnimacionesBotones.play("Botones")
 
+
+func _on_Jugar_input_event(viewport, event, shape_idx):
+	if event.is_action_released("clic"):
+		get_tree().change_scene("res://Escenas/Modo de juego/Modo_de_juego.tscn")
+
+
+func _on_Salir_input_event(viewport, event, shape_idx):
+	if event.is_action_released("clic"):
+		get_tree().quit()
